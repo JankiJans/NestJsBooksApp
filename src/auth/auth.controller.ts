d10a021dc682db4cdd4989dcb078d49e011917cb
Jan Kędzierski <jkedzierski087@gmail.com>
2023-08-17 13:29:26 +0200

    add lots of new functions to manage new users and block endpoints for non logged users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request, Response } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('/register')
-    create(@Body() userData: RegisterDTO) {
-      return this.authService.register(userData);
-    }
+  @Post('/register')
+  create(@Body() userData: RegisterDTO) {
+    return this.authService.register(userData);
+  }
 
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const tokens = await this.authService.createSession(req.user);
+    res.cookie('auth', tokens, { httpOnly: true });
+    res.send({
+      message: 'success',
+    });
+  }
 }
